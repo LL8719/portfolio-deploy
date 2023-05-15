25a5d2b2a789366b1803e1f08db8ef7f4c56c3e8
LL8719 <arsgatslr_87@yahoo.com>
2023-05-15 10:05:12 -0700

    updated title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About';
@@ -7,6 +7,9 @@ import { Footer } from './components/Footer';
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
+	useEffect(() => {
+		document.title = 'Luis Lopez Portfolio';
+	}, []);
 	return (
 		<div>
 			<Navbar />
